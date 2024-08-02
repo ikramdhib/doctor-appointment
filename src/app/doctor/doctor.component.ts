6e23aa2d349c249b547e1910c0e9b8749ac35762
dhibi <dhibikram50@gmail.com>
2024-08-02 11:15:08 +0100

    appointment table

---
+++ src/app/doctor/doctor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-doctor',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './doctor.component.html',
+  styleUrl: './doctor.component.scss'
+})
+export class DoctorComponent {
+
+}
